7f2739f2dc8d3abcabff6d1b9771b9dbd812a6af
Victor Williams <victor.williams@stu.cu.edu.ng>
2024-05-08 14:34:22 +0100

    chore:remove images route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./navbar";
 import Homepage from "./homepage";
 import Footer from "./footer";
-import Images from "./images";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/images" element={<Images />} />
       </Routes>
       <Footer />
     </>
